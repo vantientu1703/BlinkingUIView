ae89691ea1a073dd281b1e5f9323ba51847f0662
van.tien.tu <van.tien.tu@Van-Tien-Tu.local>
2017-07-31 09:29:40 +0700

    Initial commit

---
+++ BlinkingUIView/Classes/BlinkingLabel.swift
@@ -0,0 +1,23 @@
+//
+//  BlinkingLabel.swift
+//  Pods
+//
+//  Created by van.tien.tu on 7/31/17.
+//
+//
+
+import UIKit
+
+public class BlinkingLabel: UILabel {
+    public func startBlinking() {
+        let options : UIViewAnimationOptions = .repeat
+        UIView.animate(withDuration: 0.25, delay:0.0, options:options, animations: {
+            self.alpha = 0
+        }, completion: nil)
+    }
+    
+    public func stopBlinking() {
+        alpha = 1
+        layer.removeAllAnimations()
+    }
+}
--- Example/BlinkingUIView/ViewController.swift
+++ Example/BlinkingUIView/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import BlinkingUIView
 
 class ViewController: UIViewController {
 
+    var isBlinking = false
+    let blinkingLabel = BlinkingLabel(frame: CGRect(x: 10, y: 20, width: 200, height: 30))
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Setup the BlinkingLabel
+        blinkingLabel.text = "I blink!"
+        blinkingLabel.font = UIFont.systemFont(ofSize: 20)
+        view.addSubview(blinkingLabel)
+        blinkingLabel.startBlinking()
+        isBlinking = true
+        
+        // Create a UIButton to toggle the blinking
+        let toggleButton: UIButton = UIButton(frame: CGRect(x: 10, y: 65, width: 125, height: 30))
+        toggleButton.setTitle("Toggle Blinking", for: .normal)
+        toggleButton.setTitleColor(UIColor.red, for: .normal)
+        toggleButton.addTarget(self, action: #selector(ViewController.toggleBlinking), for: .touchUpInside)
+        view.addSubview(toggleButton)
     }
 
+    func toggleBlinking() {
+        if (isBlinking) {
+            blinkingLabel.stopBlinking()
+        } else {
+            blinkingLabel.startBlinking()
+        }
+        isBlinking = !isBlinking
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
